3724e4b77482ee97c7edbc19c40dffe9ba62f43d
LMW-lmw <2383250163@qq.com>
2020-05-13 15:45:38 +0800

    修改页面

--- order_consumer/src/main/resources/static/js/adminUser.js
+++ order_consumer/src/main/resources/static/js/adminUser.js
@@ -1,25 +1,95 @@
+
+var pageIndex=1;
+var maxPage=5;
+
 $(function(){
-    var userName = $(".search").html();
-    if(userName=="" || userName==null){
-        userName="null";
-    }
+    page(1);
+    var dd = new DropDown( $('#dd') );
+    $(document).click(function() {
+        // all dropdowns
+        $('.wrapper-dropdown-3').removeClass('active');
+    });
 
     $.ajax({
         type : "GET",
-        url:"/AllUsers/1/2/"+userName,
+        url:"/getAllRole",
         dataType: "json",
-        success: function (userList) {
-            userList=eval(userList);
+        success: function (List) {
+            var rolelist = eval(List[0]);
+            var allRole="";
+            $(".dropdown").html("");
+            for(var i=0;i<rolelist.length;i++){
+                var roles = rolelist[i];
+                allRole+="<li><a href='#' onclick='bianli($(this))'>"+roles.rolename+"</a></li>";
+            }
+            $(".dropdown").append(allRole);
+        }
+    })
+    $("#allRole").change(function () {
+        page(1);
+    })
+})
+function bianli(obj) {
+    alert(obj.html());
+    var x = obj.html();
+    $(".xasd").html(x);
+    alert($(".xasd").html());
+
+}
+
+function prev() {
+    if(pageIndex>1){
+        pageIndex--;
+        page(pageIndex);
+    }
+}
+
+function next() {
+    if(pageIndex<maxPage){
+        pageIndex++;
+        page(pageIndex);
+    }
+}
+
+function last() {
+    pageIndex=maxPage;
+    page(pageIndex);
+}
+
+function page(pageIndex) {
+    var userName = $(".searchbyname").val();
+    var userRole = $("#allRole").val();
+    if(userName=="" || userName==null){
+        userName=null;
+    }
+    if(userRole=="" || userRole==null){
+        userRole=0;
+    }
+    $.ajax({
+        type : "GET",
+        url:"/AllUsers/"+pageIndex+"/"+userName+"/"+userRole,
+        dataType: "json",
+        success: function (List) {
+            var totalpage = eval(List[0]);
+            pageIndex = eval(List[1]);
+            var userList = eval(List[2]);
+            alert(totalpage);
             $("#orderTable").html("");
+            $("#pageList").html("");
             var empty = "";
-            for(var i=0;i<userList.length;i++){
+            var pageList = "";
+            for (var i = 0; i < userList.length; i++) {
                 var users = userList[i];
-                empty+="<tr><td>"+users.userid+"</td><td>"+users.useraccount+"</td><td>"+users.roleinfo.rolename+"</td></tr>";
+                empty += "<tr><td class='textcetern'>" + users.userid + "</td><td class='textcetern'>" + users.useraccount + "</td><td class='textcetern'>" + users.roleinfo.rolename + "</td><td class='textcetern'><i class='iconfont green'>&#xe63e;</i><i class='iconfont orange'>&#xe60f;</i><i class='iconfont red'>&#xe608;</i></td></tr>";
+            }
+            for(var i = 1;i<=totalpage;i++){
+                pageList += "<li><a href='#' onclick='page("+i+")' id='yema'>"+i+"</a></li>";
             }
             $("#orderTable").append(empty);
+            $("#pageList").append(pageList);
         },
         error:function () {
             alert("333");
         }
     })
-})
+}
