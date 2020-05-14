0c4550deb259510aa2d7c0f44b9c4258e065a8bc
xxf <1783036950@qq.com>
2020-05-14 09:28:45 +0800

    订单控制器第一次提交

---
+++ order_provider/src/main/java/com/os/controller/OrderInfoController.java
@@ -0,0 +1,46 @@
+package com.os.controller;
+
+import com.github.pagehelper.PageHelper;
+import com.os.entity.Orderinfo;
+import com.os.service.OrderinfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class OrderInfoController {
+    @Autowired
+    OrderinfoService orderinfoService;
+
+    /**
+     * 查找所有订单
+     */
+
+    @RequestMapping("/orderInfoFindAll/{pageIndex}/{pageSize}")
+    public List orderInfoFindAll(@PathVariable("pageIndex") int pageIndex, @PathVariable("pageSize") int pageSize){
+        System.out.println("-----------------provider-- orderInfoFindAll");
+        System.out.println("-----------------provider-- pageIndex: "+pageIndex);
+        if(pageIndex==0 || pageIndex<1){
+            pageIndex=1;
+        }
+        PageHelper.startPage(pageIndex,pageSize);
+        List<Orderinfo> orderinfoList=orderinfoService.selectAll();
+        if(orderinfoList!=null && orderinfoList.size()>0){
+            int totalPage=orderinfoService.selectAll().size();
+            System.out.println("-----------------provider-- totalPage: "+totalPage);
+            int maxPage=totalPage%pageSize==0?totalPage/pageSize:totalPage/pageSize+1;
+            System.out.println("-----------------provider-- maxPage: "+maxPage);
+            List list=new ArrayList();
+            list.add(orderinfoList);
+            list.add(maxPage);
+            System.out.println(orderinfoList);
+            return list;
+        }
+        System.out.println("---------------------provider--orderInfoFindAll can't");
+        return null;
+    }
+}
